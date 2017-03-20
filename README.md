64900488f08608062d8a770339e48c130879d22e
yarhtut <yar@boost.co.nz>
2017-03-21 07:43:40 +1300

    Update README.md

--- README.md
+++ README.md
@@ -1,8 +1,7 @@
 ## SS HTTP $req and $res
 
-
 This is the example of Silverstripe simple HTTP `$req and $res` using **base controller** class.
 
-I still **did not get an idea** how to get `$req` and `$res` before **base controller** hit.
+This is just an idea how to get `$req` and `$res` with  **base controller**.
 
-
+**Currently looking into RequestFilter**
--- code/Hijack.php
+++ code/Hijack.php
@@ -2,24 +2,20 @@
 
 class Hijack extends Controller {
 
-    private static $allowed_actions = array(
-        'index'
-    );
+  public function index(SS_HTTPRequest $request) {
+    $name = ($request->getVar('name'));
+    $age = ($request->getVar('age'));
 
-    public function index(SS_HTTPRequest $request) {
-        $name = ($request->getVar('name'));
-        $age = ($request->getVar('age'));
+    $this->response->setBody(json_encode(array(
+      'name' => $age,
+      'age' => $name,
+      'message' => 'This is extends on Base Controller Class'
+    )));
 
-        $this->response->setBody(json_encode(array(
-            'name' => $age,
-            'age' => $name,
-            'message' => 'This is extneds on Base Controller Class'
-        )));
+    $this->response->addHeader("Content-type", "application/json");
 
-        $this->response->addHeader("Content-type", "application/json");
+    $this->response->setStatusCode(201);
 
-        $this->response->setStatusCode(201);
-
-        return $this->response;
-    }
+    return $this->response;
+  }
 }
